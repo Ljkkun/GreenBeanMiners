5830d79eb7bac95b97b25d1d4c363944d0a0ec98
Ljkkun <1771229850@qq.com>
2023-02-17 19:05:49 +0800

    all initialize file

---
+++ initialize/mysql.go
@@ -0,0 +1,60 @@
+package initialize
+
+import (
+	"fmt"
+	"github.com/Ljkkun/GreenBeanMiners/global"
+	"github.com/Ljkkun/GreenBeanMiners/model"
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+func MySQL() {
+
+	username := global.CONFIG.MySQLConfig.Username // 账号
+	password := global.CONFIG.MySQLConfig.Password // 密码
+	host := global.CONFIG.MySQLConfig.Host         // 数据库地址，可以是Ip或者域名
+	port := global.CONFIG.MySQLConfig.Port         // 数据库端口
+	dbName := global.CONFIG.MySQLConfig.DBName     // 数据库名
+	// dsn := "用户名:密码@tcp(地址:端口)/数据库名"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbName)
+
+	// 配置Gorm连接到MySQL
+	mysqlConfig := mysql.Config{
+		DSN:                       dsn,   // DSN
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
+	}
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Millisecond * 0, // 慢 SQL 阈值
+			LogLevel:                  logger.Info,          // 日志级别
+			IgnoreRecordNotFoundError: true,                 // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,                // 禁用彩色打印
+		},
+	)
+	if db, err := gorm.Open(mysql.New(mysqlConfig), &gorm.Config{
+		Logger: newLogger,
+	}); err == nil {
+		sqlDB, _ := db.DB()
+		sqlDB.SetMaxOpenConns(global.CONFIG.MySQLConfig.MaxOpenConns) // 设置数据库最大连接数
+		sqlDB.SetMaxIdleConns(global.CONFIG.MySQLConfig.MaxIdleConns) // 设置上数据库最大闲置连接数
+		global.DB = db
+	} else {
+		panic("connect server failed")
+	}
+
+	// 自动生成对应的数据库表
+	if global.AUTO_CREATE_DB {
+		global.DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&model.User{})
+		global.DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&model.Video{})
+		global.DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&model.Message{})
+		global.DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&model.Comment{})
+	}
+
+}
---
+++ initialize/router.go
@@ -0,0 +1,57 @@
+package initialize
+
+import (
+	"fmt"
+	"github.com/Ljkkun/GreenBeanMiners/controller"
+	"github.com/Ljkkun/GreenBeanMiners/global"
+	"github.com/Ljkkun/GreenBeanMiners/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func Router() {
+	r := gin.Default()
+	// 静态文件存放目录
+	r.Static("/static", "./public")
+
+	apiRouter := r.Group("/douyin")
+
+	// basic apis
+	apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.GET("/publish/list/", controller.PublishList)
+
+	// extra apis - I
+	apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	apiRouter.GET("/comment/list/", controller.CommentList)
+
+	// extra apis - II
+	apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+
+	// 用户权限校验
+	authed := apiRouter.Group("/")
+	authed.Use(middleware.JWT())
+	{
+		// basic apis
+		authed.GET("/user/", controller.UserInfo)
+
+		// extra apis - I
+		authed.POST("/favorite/action/", controller.FavoriteAction)
+		authed.POST("/comment/action/", controller.CommentAction)
+
+		// extra apis - II
+		authed.POST("/relation/action/", controller.RelationAction)
+	}
+
+	// 用户权限校验 + 文件格式大小检车
+	authed2 := apiRouter.Group("/")
+	authed2.Use(middleware.JWT())
+	authed2.Use(middleware.FileCheck())
+	{
+		// basic apis
+		authed2.POST("/publish/action/", controller.Publish)
+	}
+
+	r.Run(fmt.Sprintf("%s:%d", global.CONFIG.GinConfig.Host, global.CONFIG.GinConfig.Port))
+}
