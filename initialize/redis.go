10f21f0bf73722dcd48c46f12e5212b680fd0492
Ljkkun <1771229850@qq.com>
2023-02-17 20:20:31 +0800

    redis init file

---
+++ initialize/redis.go
@@ -0,0 +1,26 @@
+package initialize
+
+import (
+	"fmt"
+	"github.com/Ljkkun/GreenBeanMiners/global"
+	"github.com/Ljkkun/GreenBeanMiners/service"
+	"github.com/go-redis/redis/v8"
+)
+
+func Redis() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", global.CONFIG.RedisConfig.Host, global.CONFIG.RedisConfig.Port),
+		Password: global.CONFIG.RedisConfig.Password,
+		DB:       global.CONFIG.RedisConfig.DB,
+		PoolSize: global.CONFIG.RedisConfig.PoolSize,
+	})
+	// 检查 Redis 连通性
+	if _, err := rdb.Ping(global.CONTEXT).Result(); err != nil {
+		panic(err.Error())
+	}
+	global.REDIS = rdb
+	// 主动查询 feed，导入缓存
+	if err := service.GoFeed(); err != nil {
+		panic(err.Error())
+	}
+}
