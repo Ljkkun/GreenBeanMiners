6c3d98468094163bb615f4672eafb703a54da9c4
Ljkkun <1771229850@qq.com>
2023-02-17 20:19:18 +0800

    flex model file

--- model/user.go
+++ model/user.go
@@ -5,14 +5,15 @@ import (
 )
 
 type User struct {
-	UserID        uint64    `gorm:"column:id;primary_key;NOT NULL;autoIncrement" redis:"user_id"`
-	Name          string    `gorm:"column:name;NOT NULL" redis:"name"`
-	Password      string    `gorm:"column:password;NOT NULL" redis:"password"`
-	FollowCount   int       `gorm:"column:follow_count;NOT NULL" redis:"follow_count"`
-	FollowerCount int       `gorm:"column:follower_count;NOT NULL" redis:"follower_count"`
-	IsFollower    int       `gorm:"column:is_follower;NOT NULL" redis:"favorite_count"`
-	WorkCount     int       `gorm:"column:work_count;NOT NULL" redis:"favorite_count"`
-	FavoriteCount int       `gorm:"column:favorite_count;NOT NULL" redis:"favorite_count"`
-	CreatedAt     time.Time `gorm:"column:created_at" redis:"-"`
-	ExtInfo       *string   `gorm:"column:ext_info" redis:"-"`
+	UserID         uint64    `gorm:"column:id;primary_key;NOT NULL;autoIncrement" redis:"user_id"`
+	Name           string    `gorm:"column:name;NOT NULL" redis:"name"`
+	Password       string    `gorm:"column:password;NOT NULL" redis:"password"`
+	FollowCount    int64     `gorm:"column:follow_count;NOT NULL" redis:"follow_count"`
+	FollowerCount  int64     `gorm:"column:follower_count;NOT NULL" redis:"follower_count"`
+	IsFollower     int64     `gorm:"column:is_follower;NOT NULL" redis:"favorite_count"`
+	TotalFavorited int64     `gorm:"column:total_favorited ;NOT NULL" redis:"favorite_count"`
+	WorkCount      int64     `gorm:"column:work_count;NOT NULL" redis:"favorite_count"`
+	FavoriteCount  int64     `gorm:"column:favorite_count;NOT NULL" redis:"favorite_count"`
+	CreatedAt      time.Time `gorm:"column:created_at" redis:"-"`
+	ExtInfo        *string   `gorm:"column:ext_info" redis:"-"`
 }
--- model/video.go
+++ model/video.go
@@ -3,14 +3,16 @@ package model
 import "time"
 
 type Video struct {
-	VideoID       uint64    `gorm:"column:id;primary_key;NOT NULL" redis:"-"`
+	VideoID       uint64    `gorm:"column:video_id;primary_key;NOT NULL" redis:"-"`
+	Title         string    `gorm:"column:title;NOT NULL" redis:"title"`
+	AuthorID      uint64    `gorm:"column:author_id;index;NOT NULL" redis:"author_id"`
 	User          int       `gorm:"column:user;NOT NULL" redis:"title"`
 	PlayUrl       string    `gorm:"column:play_url;NOT NULL" redis:"play_name"`
 	CoverUrl      string    `gorm:"column:url;NOT NULL" redis:"cover_name"`
 	FavoriteCount int64     `gorm:"-" redis:"favorite_count"`
 	CommentCount  int64     `gorm:"-" redis:"comment_count"`
 	IsFavorite    uint64    `gorm:"column:is_favorite;NOT NULL" redis:"author_id"`
-	CreatedAt     time.Time `gorm:"column:created_at" redis:"-"`
+	CreatedAt     time.Time `gorm:"column:created_at;index" redis:"-"`
 	ExtInfo       *string   `gorm:"column:ext_info" redis:"-"`
 }
 
