fa5ab72680c8aaa0978f1151e15f927ba7a3b647
Ljkkun <1771229850@qq.com>
2023-02-17 18:23:18 +0800

    config

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+// GinConfig 定义 Gin 配置文件的结构体
+type GinConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+// MySQLConfig 定义 mysql 配置文件结构体
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	Username     string `mapstructure:"username"`
+	Password     string `mapstructure:"password"`
+	DBName       string `mapstructure:"db_name"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+// RedisConfig 定义 redis 配置文件结构体
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+// JWTConfig 定义 jwt 配置文件结构体
+type JWTConfig struct {
+	SigningKey string `mapstructure:"signing_key"`
+}
+
+// System 定义项目配置文件结构体
+type System struct {
+	GinConfig   *GinConfig   `mapstructure:"gin"`
+	MySQLConfig *MySQLConfig `mapstructure:"mysql"`
+	RedisConfig *RedisConfig `mapstructure:"redis"`
+	JWTConfig   *JWTConfig   `mapstructure:"jwt"`
+}
