8e1ae455b65ff9d913cb4914f80fda7f53caeca2
Ljkkun <1771229850@qq.com>
2023-02-24 02:25:38 +0800

    flex util file

--- util/video.go
+++ util/video.go
@@ -7,16 +7,22 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func GetFrame(videoPath, snapshotPath string, frameNum int) (snapshotName string, err error) {
+	command := exec.Command("ffmpeg", "-i", videoPath, "-profile:v", "main", "-movflags", "+faststart", "-crf", "26", "-y", snapshotPath)
+	command.Stdout = &bytes.Buffer{}
+	command.Stderr = &bytes.Buffer{}
+
 	buf := bytes.NewBuffer(nil)
 	err = ffmpeg.Input(videoPath).
 		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
 		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
 		WithOutput(buf, os.Stdout).
 		Run()
+
 	if err != nil {
 		log.Fatal("生成缩略图失败：", err)
 		return "", err
