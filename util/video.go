9380452217b76ac28c94cbff275e11a40aff171b
Ljkkun <1771229850@qq.com>
2023-02-27 14:54:09 +0800

    flex util file

--- util/video.go
+++ util/video.go
@@ -7,16 +7,10 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 )
 
 func GetFrame(videoPath, snapshotPath string, frameNum int) (snapshotName string, err error) {
-	command := exec.Command("ffmpeg", "-i", videoPath, "-profile:v", "main", "-movflags", "+faststart", "-crf", "26", "-y", snapshotPath)
-	command.Stdout = &bytes.Buffer{}
-	command.Stderr = &bytes.Buffer{}
-	err = command.Run()
-
 	buf := bytes.NewBuffer(nil)
 	err = ffmpeg.Input(videoPath).
 		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
@@ -35,7 +29,7 @@ func GetFrame(videoPath, snapshotPath string, frameNum int) (snapshotName string
 		return "", err
 	}
 
-	err = imaging.Save(img, snapshotPath+".png")
+	err = imaging.Save(img, snapshotPath)
 	if err != nil {
 		log.Fatal("生成缩略图失败：", err)
 		return "", err
@@ -47,7 +41,7 @@ func GetFrame(videoPath, snapshotPath string, frameNum int) (snapshotName string
 	fmt.Println("----names----", names)
 
 	// 这里把 snapshotPath 的 string 类型转换成 []string
-	snapshotName = names[len(names)-1] + ".png"
+	snapshotName = names[len(names)-1]
 	fmt.Println("----snapshotName----", snapshotName)
 	// ----snapshotName---- ./assets/testImage.png
 
